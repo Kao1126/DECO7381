654d760fb39abdc63e4b171dbd1edd69db61c622
Kevin <kevinkao1126@gmail.com>
2023-10-04 15:39:51 +1000

    add detector

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,51 @@
 const express = require('express');
 const src = require('./src');
 const path = require("path");
-
 const app = express();
+
+const SerialPort = require('serialport');
+const WebSocket = require('ws');
+
+const http = require('http');
+
+const server = http.createServer(app);
+const { ReadlineParser } = require('@serialport/parser-readline')
+// const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ port: 3000 });
+
+
+SerialPort.SerialPort.list()
+  .then(ports => {
+    ports.forEach(port => {
+      console.log(`Port: ${port.path}, Manufacturer: ${port.manufacturer || 'N/A'}`);
+    });
+  })
+  .catch(error => {
+    console.error('Error listing serial ports:', error);
+  });
+
+const port = new SerialPort.SerialPort({ path: "/dev/tty.usbmodem14201", baudRate: 9600 })
+
+const parser = new ReadlineParser();
+port.pipe(parser);
+
+wss.on('connection', (ws) => {
+    console.log('WebSocket client connected');
+    
+    // Listen for data from the serial port
+    parser.on('data', (data) => {
+        // Send the data to all connected clients
+        ws.send(data.toString());
+    });
+    ws.on('message', (message) => {
+        console.log(`Received message from client: ${message}`);
+    });
+
+    ws.on('close', () => {
+        console.log('WebSocket client disconnected');
+    });
+});
+
 
 
 app.use(express.urlencoded({extended: true}));
@@ -29,7 +72,12 @@ app.get('/data_visual', (req, res) => {
 })
 
 app.get('/interaction', (req, res) => {
+  
   res.render('interaction.ejs', {all_reef: src.all_reef, reef_bleaching: src.reef_bleaching, reef_bleaching_data: src.reef_bleaching_data});
+})
+
+app.get('/map', (req, res) => {
+  res.render('map.ejs', {all_reef: src.all_reef, reef_bleaching: src.reef_bleaching, reef_bleaching_data: src.reef_bleaching_data});
 })
 
 
